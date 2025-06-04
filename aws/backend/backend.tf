d13d4cf26af2e2f1748f72fbbc2e3026f353ec17
ne0kk <neok.kem@gmail.com>
2025-06-05 03:09:50 +0700

    New Version v1

--- aws/backend/backend.tf
+++ aws/backend/backend.tf
@@ -1,9 +1,14 @@
-# Создание объекта в существующей папке
-resource "yandex_storage_object" "backend" {
-  access_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.secret_key
-  bucket = local.bucket_name
-  key    = "terraform.tfstate"
-  source = "./terraform.tfstate"
-  depends_on = [yandex_storage_bucket.state_storage]
+terraform {
+  backend "s3" {
+    endpoints = {
+      s3 =       "https://storage.yandexcloud.net"
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gv3i9qq3bt8g400fs4/etnt60bthq02k7tlpb8e"
+    }
+    region                      = "ru-central1"
+    key                         = "terraform-remote-state"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
+}
