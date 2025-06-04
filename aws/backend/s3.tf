9f71c29ecb07dd05e6f5263663ebac66e3b929f4
ne0kk <neok.kem@gmail.com>
2025-06-04 22:26:53 +0700

    Start CI

---
+++ aws/backend/s3.tf
@@ -0,0 +1,18 @@
+# Создадим бакет с использованием ключа
+resource "yandex_storage_bucket" "state_storage" {
+  bucket     = local.bucket_name
+  access_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.secret_key
+
+  anonymous_access_flags {
+    read = false
+    list = false
+  }
+}
+
+# Локальная переменная отвечающая за текущую дату в названии бакета
+locals {
+    current_timestamp = timestamp()
+    formatted_date = formatdate("DD-MM-YYYY", local.current_timestamp)
+    bucket_name = "state-storage-${local.formatted_date}"
+}
